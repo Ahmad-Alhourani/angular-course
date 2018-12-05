df4e4cbff79658c320a728b88ab83a6b3a038f8d
Ahmad-Alhourani <ahmad.alhourani@xlabtechs.com>
2018-12-05 23:10:25 +0200

    Configuring Angular

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule()
   .catch(err => console.error(err));
